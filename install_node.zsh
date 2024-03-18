048c366a2de2f0328741926c307f28fba0efb73a
Andy Weaver <andrewayersweaver+github@gmail.com>
2024-03-18 15:50:29 -0400

    updated node install script

--- install_node.zsh
+++ install_node.zsh
@@ -17,8 +17,18 @@ curl -L $node_url -o $save_path
 chmod +x $save_path
 tar -xJf $save_path
 
+# Ensure sudo is installed
+sudo apt install -y sudo \
+|| sudo apk add --no-cache --no-check-certificate sudo \
+|| { echo "install - loc0 - Failed to install sudo. Exiting."; exit 1; }
+
 # Move folder /usr/bin/
-sudo mv $save_path $move_path
+sudo mv $save_path $move_path \
+|| mv $save_path $move_path \
+|| { echo "install - loc1 - Failed to move the folder to /usr/bin. Exiting."; exit 1; }
+
 
 # Link binary to /usr/bin/node
-sudo ln -s "${move_path}/bin/node" "/usr/bin/node"
+sudo ln -s "${move_path}/bin/node" "/usr/bin/node" \
+|| ln -s "${move_path}/bin/node" "/usr/bin/node" \
+|| { echo "install - loc2 - Failed to link the binary to /usr/bin/node. Exiting."; exit 1; }
