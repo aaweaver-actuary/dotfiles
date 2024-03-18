e9083ee1f32a04d266d70cf51f2154fae6101d92
Andy Weaver <andrewayersweaver+github@gmail.com>
2024-03-18 15:46:31 -0400

    updating so can reclone

---
+++ install_nvim.zsh
@@ -0,0 +1,72 @@
+#! /bin/zsh
+
+binary_name="nvim"
+binary_path="/usr/bin/${binary_name}"
+
+nvim_version="0.9.5"
+nvim_folder="nvim-linux64"
+tar_ext=".tar.gz"
+tar_hydrate_flag="z"
+nvim_filename="${nvim_folder}${tar_ext}"
+nvim_url="https://github.com/neovim/neovim/releases/download/v${nvim_version}/${nvim_folder}${tar_ext}"
+
+save_path="${HOME}/${nvim_filename}"
+extract_folder="${HOME}/${nvim_folder}"
+move_path="/usr/bin/${nvim_folder}"
+
+config_folder="${HOME}/.config/${binary_name}"
+
+dotfiles_folder="${HOME}/dotfiles"
+
+# Remove any previous nvim files
+rm -rf $extract_folder || true
+rm -rf $move_path || true
+rm $save_path || true
+rm $binary_path || true
+rm ${HOME}/${nvim_filename} || true
+rm -rf $dotfiles_folder || true
+rm -rf $config_folder || true
+
+# Download with curl
+curl -k -L $nvim_url -o $save_path
+
+# Extract
+chmod +x $save_path
+tar -x${tar_hydrate_flag}f $save_path
+
+# Move folder /usr/bin/
+sudo mv ${HOME}/${nvim_folder} $move_path
+
+# Link binary to /usr/bin/nvim
+sudo ln -s "${move_path}/bin/nvim" "/usr/bin/nvim"
+
+# Test nvim
+nvim --version
+
+# Clone dotfiles repo
+cd $HOME
+git clone http://github.com/aaweaver-actuary/dotfiles.git $dotfiles_folder --quiet
+
+# Extract nvim.tar.gz to ~/.config/nvim
+cd $dotfiles_folder
+mkdir -p $HOME/.config
+tar -xzf nvim.tar.gz -C $HOME/.config
+
+# Run the install_node.zsh script
+cd $dotfiles_folder
+sudo chmod +x install_node.zsh
+./install_node.zsh
+
+# Ensure node is installed
+node --version
+
+# Run the install_python.zsh script
+cd $dotfiles_folder
+sudo chmod +x install_python.zsh
+./install_python.zsh pynvim
+
+# Delete dotfiles folder
+rm -rf $dotfiles_folder
+
+# Open nvim, run LazyInstall
+nvim -c "LazyInstall" -c "qa"
---
+++ install_python.zsh
@@ -0,0 +1,92 @@
+#! /bin/env zsh
+
+# All args are assumed to be packages to install
+
+python_binary_name="py"
+python_binary_path="/usr/bin/${python_binary_name}"
+pyenv_binary_name="pyenv"
+pyenv_binary_path="/usr/bin/${pyenv_binary_name}"
+
+python_version="3.11.8"
+pyenv_folder="${HOME}/.pyenv"
+
+# Remove any previous Python files
+rm -rf $pyenv_folder || true
+rm $pyenv_binary_path || true
+
+# Ensure bash and zshell are installed
+sudo apt install -y bash zsh \
+|| sudo apk add --no-cache --no-check-certificate bash zsh \
+|| { echo "install - loc0 - Failed to install bash and/or z shell. Exiting."; exit 1; }
+
+# Install the specified version of Python and the required packages
+curl -k -L https://pyenv.run | zsh || { echo "install - loc1 - Failed to install pyenv. Exiting."; exit 1; }
+
+# Remove bash
+sudo apt remove -y bash \
+|| sudo apk del --no-cache bash \
+|| { echo "install - loc0 - Failed to remove bash. Exiting."; exit 1; }
+
+# Add the pyenv environment variables to the .zshrc file
+echo 'export PATH="${pyenv_folder}/bin:$PATH"' >> ${HOME}/.zshrc \
+|| { echo "install - loc1 - Failed to add pyenv to the .zshrc file. Exiting."; exit 1; }
+
+# Initialize pyenv on shell startup
+echo 'eval "$(pyenv init --path)"' >> ${HOME}/.zshrc \
+|| { echo "install - loc2 - Failed to initialize pyenv on shell startup. Exiting."; exit 1; }
+
+echo 'eval "$(pyenv init -)"' >> ${HOME}/.zshrc || \
+{ echo "install - loc3 - Failed to initialize pyenv on shell startup. Exiting."; exit 1; }
+
+# Reload the .zshrc file
+exec zsh
+
+# Ensure sudo is installed
+sudo apt install -y sudo \
+|| sudo apk add --no-cache --no-check-certificate sudo \
+|| { echo "install - loc4 - Failed to install sudo. Exiting."; exit 1; }
+
+# Make sure the pyenv binary is executable
+chmod +x ${pyenv_folder}/bin/pyenv \
+|| { echo "install - loc5 - Failed to make the pyenv binary executable. Exiting."; exit 1; }
+
+# Link the pyenv binary to /usr/bin
+sudo ln -s ${pyenv_folder}/bin/pyenv ${pyenv_binary_path} \
+|| { echo "install - loc6 - Failed to link the pyenv binary to /usr/bin. Exiting."; exit 1; }
+
+# Check the pyenv version
+pyenv --version || \
+{ echo "install - loc7 - Failed to check the pyenv version. Exiting."; exit 1; }
+
+# Install the specified version of Python and the required packages
+PYTHON_CONFIGURE_OPTS='--enable-optimizations --with-lto'
+PYTHON_CFLAGS='-march=native -mtune=native'
+pyenv install $python_version || \
+{ echo "install - loc8 - Failed to install Python. Exiting."; exit 1; }
+
+# Set the global version of Python
+pyenv global $python_version || \
+{ echo "install - loc9 - Failed to set global version of Python. Exiting."; exit 1; }
+
+# rehash pyenv
+pyenv rehash
+
+# Link the Python binary to /usr/bin/py
+ln -s $pyenv_folder/versions/$python_version/bin/python /usr/bin/$python_binary_name || \
+{ echo "install - loc10 - Failed to link the Python binary to /usr/bin/py. Exiting."; exit 1; }
+
+# Check the Python version
+$python_binary_name --version || \
+{ echo "install - loc11 - Failed to check the Python version. Exiting."; exit 1; }
+
+# Install the required packages (the args to this script, if any)
+$python_binary_name -m pip install --upgrade pip || \
+{ echo "install - loc12 - Failed to upgrade pip. Exiting."; exit 1; }
+
+$python_binary_name -m pip install $@ || \
+{ echo "install - loc13 - Failed to install the required packages. Exiting."; exit 1; }
+
+# Reload the .zshrc file
+exec zsh
+
+# Done
